6dd8cf6ccc65705cfc021f58137968b2acaff832
Cleo Schulten <cs@rias-institute.eu>
2021-07-30 14:05:34 +0200

    added download of the necessary spacy model

--- anonymization/entity_ruler.py
+++ anonymization/entity_ruler.py
@@ -5,25 +5,23 @@ from os import path
 class SpacyEntity:
     def __init__(self, lang, spacy_trained_model_path=""):
         self.spacy_model = ""
-        self.spacy_model = lang + "_core_web_sm"
+
+        if lang in ["zh", "en"]:
+            self.spacy_model = lang + "_core_web_sm"
+        elif lang in ["ca", "da", "nl", "fr", "de", "el", "it", "ja", "lt", "mk", "nb", "pl", "pt", "ro", "ru", "es"]:
+            self.spacy_model = lang + "_core_news_sm"
+        else:
+            self.spacy_model = "xx_ent_wiki_sm"
+            print("No language specific spacy model available. Using default model. "
+                  "Check on https://spacy.io/models how to get a model for this language.")
         try:
             if path.exists(spacy_trained_model_path + self.spacy_model):
                 self.nlp = spacy.load(spacy_trained_model_path + self.spacy_model)
             else:
+                spacy.cli.download(self.spacy_model)
                 self.nlp = spacy.load(self.spacy_model)
         except OSError:
-            self.spacy_model = lang + "_core_news_sm"
-            try:
-                self.nlp = spacy.load(self.spacy_model)
-            except OSError:
-                self.spacy_model = "xx_ent_wiki_sm"
-                try:
-                    self.nlp = spacy.load(self.spacy_model)
-                    print("No language specific spacy model available. Using default model. "
-                          "Check on https://spacy.io/models how to get a model for this language.")
-                except OSError:
-                    self.nlp = spacy.blank(lang)
-                    print("Couldn't load spacy model")
+            print("Couldn't load spacy model")
 
         # Sample text
         # text = "This is a sample number (555) 555-5555 and this too +5(555)-555-5555 +55 555 55555. abc.test@test.de"
